9e6a8ecc5f6fdd4a795ce8642b355ef123a476ca
Naiem Hasan <naiemjoy1@gmail.com>
2024-08-16 00:13:48 +0600

    get by id

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ccm0dfs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,11 +22,11 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    // Connect the client to the server
+    await client.connect();
     const productCollection = client.db("ShopEaseDB").collection("products");
 
-    //products api
+    // Products API
     app.get("/products", async (req, res) => {
       const page = parseInt(req.query.page) || 0;
       const size = parseInt(req.query.size) || 10;
@@ -41,6 +41,28 @@ async function run() {
         totalProducts,
         products,
       });
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+
+      try {
+        // Ensure the ID is a valid ObjectId
+        if (!ObjectId.isValid(id)) {
+          return res.status(400).send({ message: "Invalid product ID" });
+        }
+
+        const query = { _id: new ObjectId(id) };
+        const result = await productCollection.findOne(query);
+
+        if (result) {
+          res.send(result);
+        } else {
+          res.status(404).send({ message: "Product not found" });
+        }
+      } catch (error) {
+        res.status(500).send({ message: "Error retrieving product", error });
+      }
     });
 
     // Send a ping to confirm a successful connection
